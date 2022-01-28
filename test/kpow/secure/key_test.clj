49bb3cf1115211be3ef2d3b5c6547604c859ae20
Derek Troy-West <derek@troywest.com>
2022-01-28 15:38:51 +1100

    fix tests

--- test/kpow/secure/key_test.clj
+++ test/kpow/secure/key_test.clj
@@ -6,18 +6,18 @@
 (deftest key-gen-and-export-import
 
   ;; the same inputs generate the same key
-  (is (= (key/secret-key "aquickredfox" (.getBytes "some-salt" (StandardCharsets/UTF_8)))
-         (key/secret-key "aquickredfox" (.getBytes "some-salt" (StandardCharsets/UTF_8)))))
+  (is (= (key/secret-key "aquickredfox" "some-salt")
+         (key/secret-key "aquickredfox" "some-salt")))
 
   ;; key exported for visibility
   (is (= "//iQh9KYe7pM+mevjifZPrm7YE2+rRloG1E15zzjR88="
          (key/export-key
-          (key/secret-key "aquickredfox" (.getBytes "some-salt" (StandardCharsets/UTF_8))))))
+          (key/secret-key "aquickredfox" "some-salt"))))
 
-  ;; the default random salt determines that a different master key is generated each time for simple passphrase input
+  ;; the default random salt determines that a different  key is generated each time for simple passphrase input
   (is (not= (key/secret-key "aquickredfox")
             (key/secret-key "aquickredfox")))
 
   ;; an imported key is equivalent to one generated from inputs
   (is (= (key/import-key "//iQh9KYe7pM+mevjifZPrm7YE2+rRloG1E15zzjR88=")
-         (key/secret-key "aquickredfox" (.getBytes "some-salt" (StandardCharsets/UTF_8))))))
+         (key/secret-key "aquickredfox" "some-salt"))))
