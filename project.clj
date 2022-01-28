040e6d1b2fa9c5b16b05a99ffb7411377123ffe2
Derek Troy-West <derek@troywest.com>
2022-01-28 14:04:35 +1100

    project and gitignore

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject io.operatr/kpow-secure "1.0.0"
+
+  :description "Secure Key Generation and Payload Encryption"
+
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [org.clojure/tools.cli "1.0.206"]
+                 [ch.qos.logback/logback-classic "1.2.10"]
+  :aliases {"kaocha"       ["with-profile" "+kaocha,+smoke" "run" "-m" "kaocha.runner"]
+            "check"        ["with-profile" "+smoke" "check"]
+            "fmt"          ["with-profile" "+smoke" "cljfmt" "check"]
+            "fmtfix"       ["with-profile" "+smoke" "cljfmt" "fix"]
+            "kondo"        ["with-profile" "+smoke" "run" "-m" "clj-kondo.main" "--lint" "src:test" "--parallel"]}
+
+  :source-paths ["src"]
+  :test-paths ["test"]
+
+  :javac-options ["-target" "8" "-source" "8" "-Xlint:-options"]
+
+  :pedantic? :abort)
