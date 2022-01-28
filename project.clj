b8b4d3c29b9bb322b7804b016be16681fe8fa315
Derek Troy-West <derek@troywest.com>
2022-01-28 14:08:22 +1100

    project fix

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/tools.logging "1.2.4"]
                  [org.clojure/tools.cli "1.0.206"]
-                 [ch.qos.logback/logback-classic "1.2.10"]
+                 [ch.qos.logback/logback-classic "1.2.10"]]
+
   :aliases {"kaocha"       ["with-profile" "+kaocha,+smoke" "run" "-m" "kaocha.runner"]
             "check"        ["with-profile" "+smoke" "check"]
             "fmt"          ["with-profile" "+smoke" "cljfmt" "check"]
